2d796350a215606be4ab8c1655f2b130fb8846ca
Venura Wijenayake <venu.wije@gmail.com>
2023-08-08 19:22:56 -0700

    Delete chat directory

--- chat/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# # Register your models here.
-# from .models import User,ChatRoom,Message
-# # Register your models here
-# admin.site.register(User)
-
-# admin.site.register(ChatRoom)
-# admin.site.register(Message)
--- chat/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import ChatRoom
-
-class ChatRoomForm(ModelForm):
-  class Meta:
-    model = ChatRoom
-    fields = ['business', 'personal']
